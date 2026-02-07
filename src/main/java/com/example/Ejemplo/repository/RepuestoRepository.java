b71539c89f3dc5e63cff468aa34985c8976aca44
Josue <jobregon-2025101@kinal.edu.gt>
2026-02-07 16:19:31 -0600

    feat: Se agrego la interfaz RepuestoRepository

---
+++ src/main/java/com/example/Ejemplo/repository/RepuestoRepository.java
@@ -0,0 +1,8 @@
+package com.example.Ejemplo.repository;
+
+import com.example.Ejemplo.model.Repuesto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RepuestoRepository extends JpaRepository <Repuesto,Integer>{
+}
