5fcf7084215796c07bc5dde92f1fadcff3a083e1
Josue <jobregon-2025101@kinal.edu.gt>
2026-02-07 21:01:48 -0600

    feat: Se creo ProveedoresRepository para procedimientos CRUD de Proveedores

---
+++ src/main/java/com/example/Ejemplo/repository/ProveedoresRepository.java
@@ -0,0 +1,7 @@
+package com.example.Ejemplo.repository;
+
+import com.example.Ejemplo.model.Proveedores;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProveedoresRepository extends JpaRepository <Proveedores,Integer>{
+}
