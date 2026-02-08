e3dfb28ffc03a5fe5c22d9c2a7609886a6a0eb75
Josue <jobregon-2025101@kinal.edu.gt>
2026-02-07 23:05:20 -0600

    feat:Se agrego el Modelo Ventas para mapear a la db

---
+++ src/main/java/com/example/Ejemplo/model/Ventas.java
@@ -0,0 +1,78 @@
+package com.example.Ejemplo.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Ventas")
+public class Ventas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_venta")
+    private Integer idVenta;
+
+    @Column(name = "fecha_venta")
+    private LocalDate fechaVenta;
+
+    @Column(name = "cantidad")
+    private Integer cantidad;
+
+    @Column(name = "total")
+    private Double total;
+
+    @Column(name = "id_empleado")
+    private Integer idEmpleado;
+
+    @Column(name = "id_repuesto")
+    private Integer idRepuesto;
+
+    public Integer getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(Integer idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public LocalDate getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(LocalDate fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Integer getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Integer idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public Integer getIdRepuesto() {
+        return idRepuesto;
+    }
+
+    public void setIdRepuesto(Integer idRepuesto) {
+        this.idRepuesto = idRepuesto;
+    }
+}
